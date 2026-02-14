52008ce9567934581e395f05dedfc890c0e2fdb2
gabrielpanta <gabrielpantajimenez@hotmail.com>
2026-02-14 16:28:24 -0500

    Mejoras en la vista mesas y sidebar

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
@@ -14,12 +14,18 @@ export class LayoutComponent {
 
   role = this.auth.getRole();
   email = localStorage.getItem('email');
+  sidebarCollapsed = false;
 
   constructor(private auth: AuthService, private router: Router) {}
+
+  toggleSidebar() {
+    this.sidebarCollapsed = !this.sidebarCollapsed;
+  }
 
   logout() {
     this.auth.logout();
     this.router.navigate(['/login']);
   }
 }
+
 
